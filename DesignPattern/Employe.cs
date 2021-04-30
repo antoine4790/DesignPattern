3f01a8933f45ccd5d90fd3b19bccd1aa1dab37a1
teddy ruppin <ruppint@gmail.com>
2021-04-30 15:22:39 +0200

    Add project files.

---
+++ DesignPattern/Employe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public class Employe
+    {
+        private double salaire;
+        public int Id { get; set; } = 0;
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        
+
+        public double Salaire
+        {
+            get { return salaire; }
+            set 
+            {
+                if (value > 0)
+                    salaire = value;
+                else
+                    salaire = 1;
+            }
+        }
+
+
+        public Employe() 
+        {
+            Id = 1;
+            Nom = "";
+            Prenom = "";
+            Salaire = 1;
+        }
+
+        public Employe(int id,  string nom, string prenom, double salaire)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+            Salaire = salaire;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nom} {Prenom}";
+        }
+    }
+}
---
+++ DesignPattern/LocalStorage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public sealed class LocalStorage
+    {
+        //public List<Employe> listeEmployes = new List<Employe> { new Employe("Anderson", "Jacquouillet", 1000) };
+        public List<Employe> ListeEmployes { get; set; } = new List<Employe> { new Employe(2, "Anderson", "Jacquouillet", 1000) };
+
+        private static LocalStorage instance = null;
+        private LocalStorage() { }
+
+        //on retourne l'instance que l'on crée si elle n'existe pas
+        public static LocalStorage Instance
+        {
+            get {
+                if (instance == null)
+                    instance = new LocalStorage();
+                return instance;
+
+            }
+        }
+
+        public void AddEmploye(Employe employe) 
+        {
+            
+        }
+
+    }
+}
